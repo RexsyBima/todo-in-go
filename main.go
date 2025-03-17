1010d7677dd0f9dc5a8283bae4478e7de3fc1b07
Rexsxy Bima <Rexsy.Bimq12@gmail.com>
2025-03-17 12:50:52 +0700

    nothing much happening, just reconfiguring my nvim config instead of coding lol

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const firstName string = "Rexsy"
+	const lastName string = "Bima"
+	fmt.Println("Welcome to the todo app in Go...")
+	fmt.Println("Transitioning from python to go")
+	fmt.Printf("created by: %v %v \n", firstName, lastName)
+}
